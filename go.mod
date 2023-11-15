1f669009e3892c79eca16ff83fa7b4ee15f44351
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-15 16:53:04 +0530

    http handler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tayyab-ilyas/goserver
+
+go 1.21.4
--- main.go
+++ main.go
@@ -7,18 +7,43 @@ import (
 	"time"
 )
 
-func main(){
+func main() {
 	m := http.NewServeMux()
+	corsMux := middlewareCors(m)
+	m.HandleFunc("/", handlePage)
 
+	
+	
 	const addr = ":8000"
-
 	srv := http.Server{
-		Handler: m,
-		Addr: addr,
+		Handler:      corsMux,
+		Addr:         addr,
 		WriteTimeout: 30 * time.Second,
-		ReadTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
 	}
 	fmt.Println("server started on port ", addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Access-Central-Allow-Methods", "GET")
+	w.WriteHeader(200)
+	const page = `<html><body><p>Hello From goserver</p></body></html>`
+
+	
+	w.Write([]byte(page))
+}
+func middlewareCors(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
